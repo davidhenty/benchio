a481565d6a4c848a5fbff2f123115ae9b0298329
dsh <d.henty@epcc.ed.ac.uk>
2021-03-04 16:26:03 +0000

    Started to add file-per-node functionality

--- benchutil.f90
+++ benchutil.f90
@@ -17,7 +17,7 @@ module benchclock
 
 contains
 
-double precision function benchtime()
+  double precision function benchtime()
 
   double precision :: dummy
 
@@ -49,4 +49,96 @@ end function benchtick
 end module benchclock
 
 
+module benchnode
 
+  ! Routines to support file-per-node
+
+  use mpi
+
+  implicit none
+
+  integer :: nodecomm, nodebosscomm
+
+contains
+
+  subroutine initbenchnode(comm)
+
+    integer :: comm
+
+    integer :: size, rank, nodesize, noderank, spansize, spanrank
+    integer :: ierr, tag, colour, key, irank, namelen
+
+    integer, dimension(MPI_STATUS_SIZE) :: status
+    character*(MPI_MAX_PROCESSOR_NAME) :: nodename
+
+    call MPI_Comm_size(comm, size, ierr)
+    call MPI_Comm_rank(comm, rank, ierr)
+
+    ! Create node-local communicators
+
+    call MPI_Comm_split_type(comm, MPI_COMM_TYPE_SHARED, rank, &
+                             MPI_INFO_NULL, nodecomm, ierr)
+
+    call MPI_Comm_size(nodecomm, nodesize, ierr)
+    call MPI_Comm_rank(nodecomm, noderank, ierr)
+
+! Create spanning communicator for all the node bosses
+! Put everyone else into the same (junk) comm
+
+    colour = min(noderank, 1)
+    key    = noderank
+
+    call MPI_Comm_split(comm, colour, key, nodebosscomm, ierr)
+
+    call MPI_Comm_size(nodebosscomm, spansize, ierr)
+    call MPI_Comm_rank(nodebosscomm, spanrank, ierr)
+
+    call MPI_Get_processor_name(nodename, namelen, ierr)
+
+! Get the stats
+
+    if (rank == 0) then
+
+       if (spansize /= 1) then
+          write(*,*) "Running on ", spansize, " nodes"
+       else
+          write(*,*) "Running on ", spansize, " node"
+       end if
+
+       do irank = 0, spansize-1
+
+          tag = nodesize
+
+          if (irank /= 0) then
+
+             call MPI_Recv(nodename, MPI_MAX_PROCESSOR_NAME, MPI_CHARACTER, &
+                  irank, MPI_ANY_TAG, nodebosscomm, status, ierr)
+
+             tag = KABMPITAG(status)
+             call MPI_Get_count(status, MPI_CHARACTER, namelen, ierr)
+
+          end if
+
+          if (tag /= 1) then
+
+             write(*,*) "Node number", irank, " is ", nodename(1:namelen), &
+                  " with ", tag, " processes"
+          else
+
+             write(*,*) "Node number", irank, " is ", nodename(1:namelen), &
+                  " with ", tag, " process"
+          end if
+
+       end do
+
+    else if (noderank == 0) then
+
+       ! Send number of processes as tag
+
+       tag = nodesize
+
+       call MPI_Ssend(nodename, namelen, MPI_CHARACTER, 0, tag, nodebosscomm, ierr)
+
+    end if
+
+  end subroutine kabprintnodes
